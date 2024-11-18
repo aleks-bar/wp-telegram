13ec4151248a40deaf25ebeee9acd2f24ce8716b
AlexBar <nesferat@mail.ru>
2024-11-18 09:24:39 +0300

    change

--- post-create-script.php
+++ post-create-script.php
@@ -1,9 +1,26 @@
 <?php
-$main_composer_json = __DIR__ . '/../composer.json';
+$parent_dir = dirname(__DIR__);
+$main_composer_json = $parent_dir . '/composer.json';
 
 if (file_exists($main_composer_json)) {
     $dirname = basename(__DIR__);
     $json = json_decode(file_get_contents($main_composer_json), true);
+
+    if (empty($json['require'])) {
+        $json['require'] = [];
+    }
+
+    if (empty($json['require']['composer/installers'])) {
+        $json['require']['composer/installers'] = ['^2.0'];
+    }
+
+    if (empty($json['require']['westacks/telebot'])) {
+        $json['require']['westacks/telebot'] = ['*'];
+    }
+
+    if (empty($json['require']['wikimedia/composer-merge-plugin'])) {
+        $json['require']['wikimedia/composer-merge-plugin'] = ['*'];
+    }
 
     if (empty($json['extra'])) {
         $json['extra'] = [];
@@ -14,7 +31,15 @@ if (file_exists($main_composer_json)) {
 
     file_put_contents($main_composer_json, json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
 
-    echo "главный composer.json обновлён\n";
+    chdir($parent_dir);
+
+    exec('composer update', $output, $returnCode);
+
+    if ($returnCode === 0) {
+        echo "composer update успешно выполнен\n";
+    } else {
+        echo "Ошибка при выполнении composer update: " . implode("\n", $output) . "\n";
+    }
 } else {
     echo "главный composer.json не найден\n";
 }
